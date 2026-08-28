318cde933efabf57599a87016cce8ede2b8a8308
cs <kernkraftdev@hotmail.com>
2026-08-28 15:05:12 +0200

    Took round about 3 hours, not compeltely done yet, error handling and a bug (File Read and write) needs to be implemented, the base structure works, no compiler warnings, no UDB -Wall shows not used variables etc (obviously, because this is a test)

--- regular/1/a/binary-digits/main.c
+++ regular/1/a/binary-digits/main.c
@@ -4,43 +4,208 @@
  * @brief  Write a C-program that visualizes data in its binary represnation.
  * @version 0.1
  * @date 2026-08-28
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
+ * Allways follo the UNIX-Philosophy modularise everything make small functions to complete the big one, divide et
+ * impera
+ *
  */
 
+/**
+ * @todo implement the error handling and the file input? It doesnt work somehow   
+ * 
+ */
+#include <errno.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-char *reverse(char *s)
+#include <time.h>
+#include <unistd.h>
+/**
+ * @brief Reverses char, reason, big endian byte order
+ *
+ * @param s
+ * @return char*
+ */
+static char *reverse(char *s)
 {
-    char *rv = (char *)malloc(sizeof(char)*strlen(s));
+    char *rv = (char *)malloc(sizeof(char) * strlen(s));
 
-    for(size_t i = 0; i < strlen(s); i++)
+    if (rv == NULL)
     {
-        rv[i] = s[strlen(s)-1-i];
+        printf("An error occured!\n");
+        printf("Error (%d): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < strlen(s); i++)
+    {
+        rv[i] = s[strlen(s) - 1 - i];
     }
 
     return rv;
 }
 
-char *AsciiToBianry(char c)
+/**
+ * @brief Converts a character to a 8 bit binary
+ *
+ * @param c
+ * @return char*
+ */
+static char *AsciiToBianry(char c)
 {
-   char *rv = (char *)malloc (sizeof (char) * 9);
+    char *rv = (char *)malloc(sizeof(char) * 9);
 
-   for (int i = 0; i < 8; ++i)
-   {
-    rv[i] = (c & (1 << i) ? '1' : '0');
-   }
+    if (rv == NULL)
+    {
+        printf("An error occured!\n");
+        printf("Error (%d): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
-   return reverse(rv);
+    for (int i = 0; i < 8; ++i)
+    {
+        rv[i] = (c & (1 << i) ? '1' : '0');
+    }
+
+    return reverse(rv);
 }
 
+static void wait_nanosleep(double time)
+{
+    time_t seconds = (long)time;
+    time_t nanoseconds = (long)((time - (double)seconds) * 1e9);
+    struct timespec t1 = {seconds, nanoseconds};
+
+    if (nanosleep(&t1, NULL) == -1)
+    {
+        printf("Error (%d): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * @brief main function
+ * @todo  implement the command line parsing big question mark in my mind
+ * @param argc
+ * @param argv
+ * @return int EXIT_SUCCESS || EXIT_FAILURE
+ */
 int main(int argc, char **argv)
 {
-    printf("%s\n",AsciiToBianry('a'));
-    return 0;
-}
+    FILE *INPUTFILE;
+    if (INPUTFILE == NULL)
+    {
+        printf("Error (%d): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
+    int flags, opt, nsecs, tfnd;
+    nsecs = 0, tfnd = 0, flags = 0;
+
+    bool optionDelay = false;
+    double seconds;
+
+    bool optionOutput = false;
+    FILE *OUTPUTFILE = stdout;
+
+    if (OUTPUTFILE == NULL)
+    {
+        printf("Error (%d): %s\n", errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    while ((opt = getopt(argc, argv, "d:o:")) != -1)
+    {
+        switch (opt)
+        {
+        /**
+         *
+         * @brief d flag gets the time in nanoseconds, with wait_nanosleep()
+         * If the option -d is given, the program should wait the specified amount of seconds after each digit written.
+         *   The delay may be specified as an ASCII represented decimal floating point number, e.g. 0.5.
+         * also use the strtod() function to convert the time
+         */
+        case 'd':
+            /**
+             * @todo write error handling
+             *
+             */
+            seconds = strtof(optarg, NULL);
+            break;
+
+        /**
+         * @brief If the option -o is given, the output is written to the specified file outfile. Otherwise, the output
+         *  is written to stdout
+         *
+         */
+        case 'o':
+
+            /**
+             * @todo implement error handling
+             * 
+             */
+            OUTPUTFILE = fopen(optarg, "w+");
+            break;
+
+        /**
+         * @brief basic error and wrong input handeling
+         *
+         */
+        case ':':
+            printf("Error no Arguments given");
+            fprintf(stderr, "Usage: %s [-d DELAY] [-o OUTPUTFILE] [FILE]\n", argv[0]);
+            exit(EXIT_FAILURE);
+            break;
+
+        case '?':
+            printf("ERROR: unknown arg %c\n", optopt);
+            fprintf(stderr, "Usage: %s [-d DELAY] [-o OUTPUTFILE] [FILE]\n", argv[0]);
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
+
+    if (optind >= argc)
+    {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    do
+    {
+
+        if ((argc - optind) == 0)
+            INPUTFILE = stdin;
+
+        else
+            INPUTFILE = fopen(argv[optind], "r");
+
+        int c;
+        while ((c = fgetc(INPUTFILE)) != EOF)
+        {
+            fputs(AsciiToBianry(c), OUTPUTFILE);
+
+            if (optionDelay)
+            {
+                fflush(OUTPUTFILE);
+                wait_nanosleep(seconds);
+            }
+        }
+        fprintf(OUTPUTFILE, "\n");
+        fclose(INPUTFILE);
+    } while (++optind < argc);
+
+    fclose(INPUTFILE);
+    fclose(OUTPUTFILE);
+
+    free(INPUTFILE);
+    free(OUTPUTFILE);
+
+    exit(EXIT_SUCCESS);
+}
