a0e9914764b5413283c6c7494f5196ab2828f799
cs <kernkraftdev@hotmail.com>
2026-09-10 13:21:46 +0200

    cleaned up

--- regular/1/a/mysort/mysort.c
+++ regular/1/a/mysort/mysort.c
@@ -11,8 +11,8 @@
  * <https://linux.die.net/man/3/qsort> for further details
  */
 #include <assert.h>
-#include <ctype.h>
 #include <errno.h>
+#include <getopt.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -23,134 +23,133 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define error(msg, program_name)                                                                                       \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        printf("%s\n", msg);                                                                                           \
-        fprintf(stderr, "Usage: %s [-r] [file1]...\n", program_name);                                                  \
-        printf("Error (%d): %s\n", errno, strerror(errno));                                                            \
-        exit(EXIT_FAILURE);                                                                                            \
+#define error(msg, program_name)					\
+    do									\
+    {									\
+        printf("%s\n", msg);						\
+        fprintf(stderr, "Usage: %s [-r] [file1]...\n", program_name);	\
+        printf("Error (%d): %s\n", errno, strerror(errno));		\
+        exit(EXIT_FAILURE);						\
     } while (0)
 
-typedef struct options_t
-{
-    bool reverse;
+typedef struct options_t {
+	bool reverse;
 } options_t;
 
-typedef struct lines_t
-{
-    char **data;
-    size_t count;
-    size_t capacity;
+typedef struct lines_t {
+	char **data;
+	size_t count;
+	size_t capacity;
 } lines_t;
 
-static int cmpstringp(const void *p1, const void *p2)
+static int
+cmpstringp(const void *p1, const void *p2)
 {
     /* The actual arguments to this function are "pointers to
        pointers to char", but strcmp(3) arguments are "pointers
        to char", hence the following cast plus dereference */
 
-    return strcmp(*(char *const *)p1, *(char *const *)p2);
+	return strcmp(*(char *const *)p1, *(char *const *)p2);
 }
 
-static int cmpstringp_rev(const void *p1, const void *p2)
+static int
+cmpstringp_rev(const void *p1, const void *p2)
 {
-    return cmpstringp(p2, p1);
+	return cmpstringp(p2, p1);
 }
 
-int readLines(FILE *input, lines_t *lines)
+int
+readLines(FILE *input, lines_t *lines)
 {
-    if (input == NULL)
-    {
-        error("INPUT EMPTY ERROR!", "readLines()");
-        return -1;
-    }
+	if (input == NULL) {
+		error("INPUT EMPTY ERROR!", "readLines()");
+		return -1;
+	}
 
-    char *line = NULL;
-    size_t line_buffer_len = 0;
-    ssize_t read_size = 0;
+	char *line = NULL;
+	size_t line_buffer_len = 0;
+	ssize_t read_size = 0;
 
-    while ((read_size = getline(&line, &line_buffer_len, input)) != -1)
-    {
-        if (read_size > 0 && line[read_size - 1] == '\n')
-            line[read_size - 1] = '\0';
+	while ((read_size = getline(&line, &line_buffer_len, input)) != -1) {
+		if (read_size > 0 && line[read_size - 1] == '\n')
+			line[read_size - 1] = '\0';
 
-        if (lines->count == lines->capacity)
-        {
-            // initialize base capacity
-            lines->capacity = lines->capacity ? lines->capacity * 2 : 128;
-            lines->data = realloc(lines->data, lines->capacity * sizeof(char *));
-            if (lines->data == NULL)
-                error("realloc failed", "readLines()");
-        }
+		if (lines->count == lines->capacity) {
+	    // initialize base capacity
+			lines->capacity = lines->capacity ? lines->capacity *
+			    2 : 128;
+			lines->data = realloc(lines->data,
+			    lines->capacity * sizeof(char *));
+			if (lines->data == NULL)
+				error("realloc failed", "readLines()");
+		}
 
-        lines->data[lines->count++] = strdup(line);
-    }
+		lines->data[lines->count++] = strdup(line);
+	}
 
-    free(line);
-    return 0;
+	free(line);
+	return 0;
 }
 
-void sortAndPrint(lines_t *lines, FILE *output, bool reverse)
+void
+sortAndPrint(lines_t *lines, FILE *output, bool reverse)
 {
-    qsort(lines->data, lines->count, sizeof(char *), reverse ? cmpstringp_rev : cmpstringp);
+	qsort(lines->data, lines->count, sizeof(char *),
+	    reverse ? cmpstringp_rev : cmpstringp);
 
-    for (size_t i = 0; i < lines->count; ++i)
-    {
-        fprintf(output, "%s\n", lines->data[i]);
-        free(lines->data[i]);
-    }
-    free(lines->data);
+	for (size_t i = 0; i < lines->count; ++i) {
+		fprintf(output, "%s\n", lines->data[i]);
+		free(lines->data[i]);
+	}
+	free(lines->data);
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    FILE *INPUT, *OUTPUTFILE = stdout;
+	FILE *INPUT, *OUTPUTFILE = stdout;
 
-    if (OUTPUTFILE == NULL)
-        error("Failed to allocate OUPUTFILE", "OUTPUTFILE");
+	if (OUTPUTFILE == NULL)
+		error("Failed to allocate OUPUTFILE", "OUTPUTFILE");
 
-    int option;
-    bool reverse = false;
+	int option;
+	bool reverse = false;
 
-    while ((option = getopt(argc, argv, "r")) != -1)
-    {
-        switch (option)
-        {
-        case 'r':
-            reverse = true;
-            break;
-        case ':':
-            error("Error no Arguments given", argv[0]);
-            break;
+	while ((option = getopt(argc, argv, "r")) != -1) {
+		switch (option) {
+		case 'r':
+			reverse = true;
+			break;
+		case ':':
+			error("Error no Arguments given", argv[0]);
+			break;
 
-        case '?':
-            error("unknown arg", argv[0]);
-            break;
-        }
-    }
+		case '?':
+			error("unknown arg", argv[0]);
+			break;
+		}
+	}
 
-    options_t opts = {.reverse = reverse};
-    lines_t lines = {.data = NULL, .count = 0, .capacity = 0};
+	options_t opts = {.reverse = reverse};
+	lines_t lines = {.data = NULL, .count = 0, .capacity = 0};
 
-    do
-    {
-        if ((argc - optind) == 0)
-            INPUT = stdin;
-        else
-            INPUT = fopen(argv[optind], "r+");
+	do {
+		if ((argc - optind) == 0)
+			INPUT = stdin;
+		else
+			INPUT = fopen(argv[optind], "r+");
 
-        if (INPUT == NULL)
-            error("failed to allocate the INPUTFILE", "main()");
+		if (INPUT == NULL)
+			error("failed to allocate the INPUTFILE", "main()");
 
-        readLines(INPUT, &lines);
+		readLines(INPUT, &lines);
 
-        fclose(INPUT);
-    } while (++optind < argc);
+		fclose(INPUT);
+	} while (++optind < argc);
 
-    sortAndPrint(&lines, OUTPUTFILE, opts.reverse);
-    fflush(OUTPUTFILE);
+	sortAndPrint(&lines, OUTPUTFILE, opts.reverse);
+	fflush(OUTPUTFILE);
 
-    fclose(OUTPUTFILE);
-    exit(EXIT_SUCCESS);
+	fclose(OUTPUTFILE);
+	exit(EXIT_SUCCESS);
 }
